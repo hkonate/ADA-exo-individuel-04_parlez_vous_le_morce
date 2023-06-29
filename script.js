fe267dd1faf2448348aa5b721951d6043745b701
hkonate <konate.tronca@gmail.com>
2023-06-29 14:50:45 +0200

    step 3

--- script.js
+++ script.js
@@ -55,10 +55,23 @@ const morseToLatin = {
   "...-": "V",
   "....": "H",
 };
-
+// Srzp 1
 const getLatinCharacterList = (str) => str.split("");
-
+// Step 2
 const translateLatinCharacter = (char) => latinToMorse[char];
-
+// Step 3
+const encode = (txt) => {
+  const upperCaseTxt = getLatinCharacterList(txt.toUpperCase());
+  const encodeTxt = [];
+  for (i = 0; i < upperCaseTxt.length; i++) {
+    if (
+      upperCaseTxt.join("").charCodeAt(i) >= 65 &&
+      upperCaseTxt.join("").charCodeAt(i) <= 90
+    )
+      encodeTxt.push(translateLatinCharacter(upperCaseTxt[i]));
+  }
+  return encodeTxt;
+};
 console.log(getLatinCharacterList("Hello, world"));
 console.log(translateLatinCharacter("A"));
+console.log(encode("Hello, world"));
