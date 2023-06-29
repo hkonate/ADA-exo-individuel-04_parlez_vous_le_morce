f9917931ed8bd741fc9a656fbc6acaccb48eb662
hkonate <konate.tronca@gmail.com>
2023-06-29 14:10:57 +0200

    step 2done

--- script.js
+++ script.js
@@ -1,2 +1,64 @@
+const latinToMorse = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+};
+
+const morseToLatin = {
+  "-": "T",
+  "--": "M",
+  "---": "O",
+  "--.": "G",
+  "--.-": "Q",
+  "--..": "Z",
+  "-.": "N",
+  "-.-": "K",
+  "-.--": "Y",
+  "-.-.": "C",
+  "-..": "D",
+  "-..-": "X",
+  "-...": "B",
+  ".": "E",
+  ".-": "A",
+  ".--": "W",
+  ".---": "J",
+  ".--.": "P",
+  ".-.": "R",
+  ".-..": "L",
+  "..": "I",
+  "..-": "U",
+  "..-.": "F",
+  "...": "S",
+  "...-": "V",
+  "....": "H",
+};
+
 const getLatinCharacterList = (str) => str.split("");
+
+const translateLatinCharacter = (char) => latinToMorse[char];
+
 console.log(getLatinCharacterList("Hello, world"));
+console.log(translateLatinCharacter("A"));
